124e61194035be06f4b4afb4ca02ec515111eeb0
Stephen MacNeil <smacneil01@gmail.com>
2014-09-19 19:38:39 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var telnet = require('net')
+var gpsCoordinates = require('./gps.json').data
+
+//configured for android use
+var client = telnet.connect('5554', 'localhost')
+ 
+var startPolling = function () {
+   
+    //cycle through list sending gps to android emulator
+    setInterval(function () {
+        elem = gpsCoordinates.shift()
+        console.log(elem)
+        console.log('geo fix ' + elem.lon + ' '+ elem.lat)
+        client.write('geo fix ' + elem.lon + ' ' + elem.lat + '\n')
+        gpsCoordinates.push(elem)
+    
+    }, 3000)
+}
+
+//after the first set of data is received
+client.on('data', function(data) {
+    console.log(data.toString())
+
+}).on('connect', function() {
+    startPolling() 
+    console.log("connected")
+}).on('end', function() {
+    console.log('disconnected')
+});
