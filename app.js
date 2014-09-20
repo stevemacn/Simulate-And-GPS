8269c9eab90b738f8fc040d219ad13e130e634f5
Stephen MacNeil <smacneil01@gmail.com>
2014-09-19 20:07:23 -0400

    allow user to specify frequencies

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var telnet = require('net')
-var gpsCoordinates = require('./gps.json').data
+
+//set default values
+var gpsData = require('./gps.json')
+var gpsCoordinates = gpsData.data 
+var gpsFrequency = gpsData.frequency || 3000
 
 //configured for android use
 var client = telnet.connect('5554', 'localhost')
- 
-var startPolling = function () {
-   
+
+var startPolling = function() {
+
     //cycle through list sending gps to android emulator
-    setInterval(function () {
+    setInterval(function() {
         elem = gpsCoordinates.shift()
         console.log(elem)
-        console.log('geo fix ' + elem.lon + ' '+ elem.lat)
+        console.log('geo fix ' + elem.lon + ' ' + elem.lat)
         client.write('geo fix ' + elem.lon + ' ' + elem.lat + '\n')
         gpsCoordinates.push(elem)
-    
+
     }, 3000)
 }
 
@@ -22,7 +26,7 @@ client.on('data', function(data) {
     console.log(data.toString())
 
 }).on('connect', function() {
-    startPolling() 
+    startPolling()
     console.log("connected")
 }).on('end', function() {
     console.log('disconnected')
